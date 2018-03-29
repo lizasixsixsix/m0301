c28d986640a27eb565c87489d7cfdd70c73747a3
lizasixsixsix <lizasixsixsix@gmail.com>
2018-03-29 16:51:45 +0300

    extension method to execute any query implemented

--- Task/LinqSamples.cs
+++ Task/LinqSamples.cs
@@ -4,61 +4,83 @@
 //
 //Copyright (C) Microsoft Corporation.  All rights reserved.
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Windows.Forms;
-using System.Xml.Linq;
 using SampleSupport;
-using Task.Data;
-using System.Data.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-// Version Mad01
+using Task.Data;
 
 namespace SampleQueries
 {
-	[Title("LINQ Module")]
-	[Prefix("Linq")]
-	public class LinqSamples : SampleHarness
-	{
-		private DataSource dataSource = new DataSource();
+    [Title("LINQ Module")]
+    [Prefix("Linq")]
+    public class LinqSamples : SampleHarness
+    {
+        private DataSource dataSource = new DataSource();
 
-		[Category("Restriction Operators")]
-		[Title("Where - Task 1")]
-		[Description("This sample uses the where clause to find all elements of an array with a value less than 5.")]
-		public void Linq1()
-		{
-			int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+        [Category("Restriction Operators")]
+        [Title("Where - Task 1")]
+        [Description("This sample uses the where clause to find all elements of an array with a value less than 5.")]
+        public void Linq1()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 
-			var lowNums =
-				from num in numbers
-				where num < 5
-				select num;
+            var lowNums =
+                from num in numbers
+                where num < 5
+                select num;
 
-			Console.WriteLine("Numbers < 5:");
+            Console.WriteLine("Numbers < 5:");
 
-			foreach (var x in lowNums)
-			{
-				Console.WriteLine(x);
-			}
-		}
+            foreach (var x in lowNums)
+            {
+                Console.WriteLine(x);
+            }
+        }
 
-		[Category("Restriction Operators")]
-		[Title("Where - Task 2")]
-		[Description("This sample return return all presented in market products")]
-		public void Linq2()
-		{
-			var products =
-				from p in dataSource.Products
-				where p.UnitsInStock > 0
-				select p;
+        [Category("Restriction Operators")]
+        [Title("Where - Task 2")]
+        [Description("This sample return return all presented in market products")]
+        public void Linq2()
+        {
+            var products =
+                from p in dataSource.Products
+                where p.UnitsInStock > 0
+                select p;
 
-			foreach (var p in products)
-			{
-				ObjectDumper.Write(p);
-			}
-		}
-	}
+            foreach (var p in products)
+            {
+                ObjectDumper.Write(p);
+            }
+        }
+
+        [Category("My Tasks")]
+        [Title("Task 01")]
+        [Description("Reusable Query")]
+        public void Linq01()
+        {
+            var query = new Func<DataSource, int, IEnumerable<Customer>>(
+                (source, x) =>
+                    source.Customers.Where(
+                        c => c.Orders.Select(o => o.Total).Sum() > x));
+
+            var customers = dataSource.Execute(
+                source => query(source, 500));
+
+            foreach (var c in customers)
+            {
+                ObjectDumper.Write(c);
+            }
+        }
+    }
+
+    public static class DataSourceExtensions
+    {
+        public static TResult Execute<TResult>(this DataSource source,
+                                               Func<DataSource, TResult> stuff)
+        {
+            return stuff(source);
+        }
+    }
 }
